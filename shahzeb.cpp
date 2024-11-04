a280ffe599fac376147d041ea5f5b72779dc7a7e
shahzebsheikh986 <shahzebsheikh986@gmail.com>
2024-11-04 17:38:06 +0530

    new change

--- shahzeb.cpp
+++ shahzeb.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
-int main(){
-  unsigned int {2};
-  
+
+int main() {
+    string a = "bitsat";
+   cout<<a<<"\n";
+    getline(cin, a);  // Reads input from the user and stores it in 'a'
+    cout << "You entered: " << a << endl;
 
     return 0;
 }
+
